605a43e15edbdd56b9a15dee9443c8ec9dc60970
Manabu Ishii <manabu.ishii.rb@gmail.com>
2021-08-20 01:24:51 +0900

    Add struct object

--- main.go
+++ main.go
@@ -41,6 +41,28 @@ type simpleSchema struct {
 	SampleList []*Sample `json:"samplelist"`
 }
 
+//
+type PathObject struct {
+	Path   string `json:"path"`
+	Format string `json:"format"`
+}
+
+type referenceSchema struct {
+	Name                           string      `json:"name"`
+	Reference                      *PathObject `json:"reference"`
+	SortsamMaxRecordsInRam         int         `json:"sortsam_max_records_in_ram"`
+	SortsamJavaOptions             string      `json:"sortsam_java_options"`
+	BwaNumThreads                  int         `json:"bwa_num_threads"`
+	BwaBasesPerBatch               int         `json:"bwa_bases_per_batch"`
+	UseBqsr                        bool        `json:"use_bqsr"`
+	Dbsnp                          *PathObject `json:"dbsnp"`
+	Mills                          *PathObject `json:"mills"`
+	KnownIndels                    *PathObject `json:"known_indels"`
+	SamtoolsNumThreads             int         `json:"samtools_num_threads"`
+	Gatk4HaplotypeCallerNumThreads int         `json:"gatk4_HaplotypeCaller_num_threads"`
+	BgzipNumThreads                int         `json:"bgzip_num_threads"`
+}
+
 func md5File(filePath string) (string, error) {
 	file, err := os.Open(filePath)
 
@@ -158,19 +180,43 @@ func main() {
 			fmt.Printf("- %s\n", desc)
 		}
 	}
-	// fmt.Println("Load sample")
-	// raw, err := ioutil.ReadFile(os.Args[2])
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	os.Exit(1)
-	// }
+	fmt.Println("Load sample")
+	rraw, err := ioutil.ReadFile(os.Args[4])
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 
-	// var ss simpleSchema
+	var rss referenceSchema
 
-	// json.Unmarshal(raw, &ss)
-	// fmt.Println("Load end")
-	// fmt.Printf("Name is [%s]\n", ss.Name)
-	// fmt.Printf("Fq1 is [%s]\n", ss.Fq1)
+	json.Unmarshal(rraw, &rss)
+	fmt.Println("Load end")
+	fmt.Println("")
+	fmt.Printf("reference:\n")
+	fmt.Printf("  class: File\n")
+	fmt.Printf("  path: %s\n", rss.Reference.Path)
+	fmt.Printf("  format: http://edamontology.org/format_1929\n")
+	fmt.Printf("sortsam_max_records_in_ram: %d\n", rss.SortsamMaxRecordsInRam)
+	fmt.Printf("sortsam_java_options: %s\n", rss.SortsamJavaOptions)
+	fmt.Printf("bwa_num_threads: %d\n", rss.BwaNumThreads)
+	fmt.Printf("bwa_bases_per_batch: %d\n", rss.BwaBasesPerBatch)
+	fmt.Printf("use_bqsr: %t\n", rss.UseBqsr)
+	fmt.Printf("dbsnp:\n")
+	fmt.Printf("  class: File\n")
+	fmt.Printf("  path: %s\n", rss.Dbsnp.Path)
+	fmt.Printf("  format: http://edamontology.org/format_3016\n")
+	fmt.Printf("mills:\n")
+	fmt.Printf("  class: File\n")
+	fmt.Printf("  path: %s\n", rss.Mills.Path)
+	fmt.Printf("  format: http://edamontology.org/format_3016\n")
+	fmt.Printf("known_indels:\n")
+	fmt.Printf("  class: File\n")
+	fmt.Printf("  path: %s\n", rss.KnownIndels.Path)
+	fmt.Printf("  format: http://edamontology.org/format_3016\n")
+	fmt.Printf("samtools_num_threads: %d\n", rss.SamtoolsNumThreads)
+	fmt.Printf("gatk4_HaplotypeCaller_num_threads: %d\n", rss.Gatk4HaplotypeCallerNumThreads)
+	fmt.Printf("bgzip_num_threads: %d\n", rss.BgzipNumThreads)
+
 	// fmt.Printf("Md5 is [%s]\n", ss.Md5)
 
 	// // validate
