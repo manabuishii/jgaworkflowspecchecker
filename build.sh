fbbb02030f996ecd3cfa1e6ffdf297655f61aa6a
Manabu Ishii <manabu.ishii.rb@gmail.com>
2021-09-11 10:48:21 +0900

    interval_listに対応 Fixes #25

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=0.8.0
+VERSION=0.9.0
 
 
 env GOOS=darwin GOARCH=amd64 go build -o jgaworkflowmanager-mac-${VERSION} -ldflags "-X main.version=${VERSION} -X main.revision=$(git rev-parse --short HEAD)" main.go
