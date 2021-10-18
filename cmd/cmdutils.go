edaa4fce1a511375c90fab7d11243d6acebf07e5
Manabu Ishii <manabu.ishii.rb@gmail.com>
2021-10-18 16:37:30 +0900

    ファイルパスの文字列に問題があるときの出力追加 #86

--- cmd/cmdutils.go
+++ cmd/cmdutils.go
@@ -50,6 +50,20 @@ func loadSampleSheetAndConfigFile(args []string) bool {
 		fmt.Println("Some required files are missing. So stop execute")
 		return false
 	}
+	//
+	allfilepathisvalidchar := true
+	if !utils.IsOnlyValidCharcterInFilepath(samplesheet_data_file) {
+		fmt.Printf("[%s] has invalid character.\n", samplesheet_data_file)
+		allfilepathisvalidchar = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(config_data_file) {
+		fmt.Printf("[%s] has invalid character.\n", config_data_file)
+		allfilepathisvalidchar = false
+	}
+	if !allfilepathisvalidchar {
+		fmt.Println("Some required files has invalid character. So stop execute")
+		return false
+	}
 	// files are provided. check both files contents.
 	validateisfine := true
 	// sample sheet schema provided by embed.
@@ -121,5 +135,93 @@ func loadSampleSheetAndConfigFile(args []string) bool {
 	if displayMeesage {
 		fmt.Println("Load config file end")
 	}
+	validateisfine = validateisfine && IsAllSamplesheetFilepathHasValidchar(&ss)
+	validateisfine = validateisfine && IsAllFilepathInConfigFileHasValidchar(&rss)
 	return validateisfine
+}
+
+func IsAllSamplesheetFilepathHasValidchar(samplesheet *utils.SimpleSchema) bool {
+	result := true
+	for _, s := range ss.SampleList {
+		for _, r := range s.RunList {
+			if r.PEOrSE == "PE" {
+				// PE
+				if !utils.IsOnlyValidCharcterInFilepath(r.FQ1) {
+					fmt.Printf("In SampleID[%s] RunID[%s] [%s] has invalid character in filepath\n", s.SampleId, r.RunId, r.FQ1)
+					result = false
+				}
+				if !utils.IsOnlyValidCharcterInFilepath(r.FQ2) {
+					fmt.Printf("In SampleID[%s] RunID[%s] [%s] has invalid character in filepath\n", s.SampleId, r.RunId, r.FQ2)
+					result = false
+				}
+			} else {
+				// SE
+				if !utils.IsOnlyValidCharcterInFilepath(r.FQ1) {
+					fmt.Printf("In SampleID[%s] RunID[%s] [%s] has invalid character in filepath\n", s.SampleId, r.RunId, r.FQ1)
+					result = false
+				}
+			}
+		}
+	}
+	return result
+}
+
+func IsAllFilepathInConfigFileHasValidchar(rss *utils.ReferenceSchema) bool {
+	result := true
+	if !utils.IsOnlyValidCharcterInFilepath(rss.WorkflowFile.Path) {
+		fmt.Printf("In config file, `workflow_file` path [%s] has invalid character.\n", rss.WorkflowFile.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.OutputDirectory.Path) {
+		fmt.Printf("In config file, `output_directory` path [%s] has invalid character.\n", rss.OutputDirectory.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.ContainerCacheDirectory.Path) {
+		fmt.Printf("In config file, `container_cache_directory` path [%s] has invalid character.\n", rss.ContainerCacheDirectory.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.Reference.Path) {
+		fmt.Printf("In config file, `reference` path [%s] has invalid character.\n", rss.Reference.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.Dbsnp.Path) {
+		fmt.Printf("In config file, `dnsnp` path [%s] has invalid character.\n", rss.Dbsnp.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.Mills.Path) {
+		fmt.Printf("In config file, `mills` path [%s] has invalid character.\n", rss.Mills.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.KnownIndels.Path) {
+		fmt.Printf("In config file, `known_indels` path [%s] has invalid character.\n", rss.KnownIndels.Path)
+		result = false
+	}
+	// Autosome PAR
+	if !utils.IsOnlyValidCharcterInFilepath(rss.HaplotypecallerAutosomePARIntervalBed.Path) {
+		fmt.Printf("In config file, `haplotypecaller_autosome_PAR_interval_bed` path [%s] has invalid character.\n", rss.HaplotypecallerAutosomePARIntervalBed.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.HaplotypecallerAutosomePARIntervalList.Path) {
+		fmt.Printf("In config file, `haplotypecaller_autosome_PAR_interval_list` path [%s] has invalid character.\n", rss.HaplotypecallerAutosomePARIntervalList.Path)
+		result = false
+	}
+	// ChrX NonPAR
+	if !utils.IsOnlyValidCharcterInFilepath(rss.KnownIndels.Path) {
+		fmt.Printf("In config file, `haplotypecaller_chrX_nonPAR_interval_bed` path [%s] has invalid character.\n", rss.HaplotypecallerChrXNonPARIntervalBed.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.KnownIndels.Path) {
+		fmt.Printf("In config file, `haplotypecaller_chrX_nonPAR_interval_list` path [%s] has invalid character.\n", rss.HaplotypecallerChrXNonPARIntervalList.Path)
+		result = false
+	}
+	// ChrY NonPar
+	if !utils.IsOnlyValidCharcterInFilepath(rss.KnownIndels.Path) {
+		fmt.Printf("In config file, `haplotypecaller_chrY_nonPAR_interval_bed` path [%s] has invalid character.\n", rss.HaplotypecallerChrYNonPARIntervalBed.Path)
+		result = false
+	}
+	if !utils.IsOnlyValidCharcterInFilepath(rss.KnownIndels.Path) {
+		fmt.Printf("In config file, `haplotypecaller_chrY_nonPAR_interval_list` path [%s] has invalid character.\n", rss.HaplotypecallerChrYNonPARIntervalList.Path)
+		result = false
+	}
+	return result
 }
